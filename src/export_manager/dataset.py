b9cee6368e1fc9791262e0088e20ff55744b39fc
Jacob Williams <jacobaw@gmail.com>
2020-04-08 15:15:44 -0700

    read/write metrics

--- src/export_manager/dataset.py
+++ src/export_manager/dataset.py
@@ -1,5 +1,8 @@
+import csv
 from git import Repo
+from operator import itemgetter
 from pathlib import Path
+import toml
 
 
 DEFAULT_GITIGNORE = """.DS_Store
@@ -13,7 +16,7 @@ DEFAULT_CONFIG_TOML = """# cmd = "echo example > $PARCEL_DEST.txt"
 # interval = "1 day"
 """
 
-INITIAL_METRICS_CSV = "id,files,bytes"
+INITIAL_METRICS_CSV = "parcel_id,files,bytes"
 
 
 class DatasetDir:
@@ -44,8 +47,52 @@ class DatasetDir:
             gitignore_path = self.path.joinpath('.gitignore')
             if not gitignore_path.exists():
                 gitignore_path.write_text(DEFAULT_GITIGNORE)
+            self.commit('[export_manager] initialize',
+                        ['.gitignore', 'config.toml', 'metrics.csv'])
+
+    def read_config(self):
+        if not self.config_path.is_file():
+            raise Exception(f'{self.config_path} is not a file')
+        return toml.load(self.config_path)
+
+    def is_git(self):
+        return self.read_config().get('git', False)
+
+    def commit(self, message, paths):
+        if self.is_git():
             repo = Repo(str(self.path))
-            index = repo.index
-            index.add(['.gitignore', 'config.toml', 'metrics.csv'])
+            repo.index.add(paths)
             if repo.is_dirty():
-                index.commit('[export_manager] initialize')
+                repo.index.commit(message)
+
+    def read_metrics(self):
+        if not self.metrics_path.is_file():
+            return {}
+
+        results = {}
+        with open(self.metrics_path) as file:
+            for row in csv.DictReader(file):
+                parcel_id = row['parcel_id']
+                if parcel_id in results:
+                    raise Exception('parcel_id appears multiple times in '
+                                    + f'metrics.csv: {parcel_id}')
+                results[parcel_id] = row
+        return results
+
+    def update_metrics(self, updates):
+        metrics = self.read_metrics()
+        fields = set()
+        if metrics:
+            fields.update(next(iter(metrics.values())).keys())
+        for row in updates.values():
+            fields.update(row.keys())
+            metrics[row['parcel_id']] = row
+        rows = sorted(metrics.values(), key=itemgetter('parcel_id'))
+
+        with open(self.metrics_path, 'w') as file:
+            writer = csv.DictWriter(file, fieldnames=fields)
+            writer.writeheader()
+            writer.writerows(rows)
+
+        self.commit('[export_manager] update metrics', ['metrics.csv'])
+
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,8 +1,17 @@
+from contextlib import contextmanager
 from git import Repo
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from export_manager import dataset
 from export_manager.dataset import DatasetDir
+
+
+@contextmanager
+def tempdatasetdir(git=False):
+    with TemporaryDirectory() as rawpath:
+        dsd = DatasetDir(rawpath)
+        dsd.initialize(git=git)
+        yield dsd
 
 
 def test_initialize():
@@ -34,3 +43,62 @@ def test_initialize_git():
                 == ['.gitignore', 'config.toml', 'metrics.csv'])
         assert (path.joinpath('config.toml').read_text()
                 == dataset.DEFAULT_CONFIG_TOML + 'git = true\n')
+
+
+def test_read_config():
+    with tempdatasetdir() as dsd:
+        dsd.config_path.write_text('cmd = "echo hi"')
+        assert dsd.read_config() == {'cmd': 'echo hi'}
+
+
+def test_read_metrics_nonexistent():
+    with tempdatasetdir() as dsd:
+        dsd.metrics_path.unlink()
+        assert dsd.read_metrics() == {}
+
+
+def test_update_metrics():
+    with tempdatasetdir() as dsd:
+        initial = {
+            '1999-01-02T030405Z': {
+                'parcel_id': '1999-01-02T030405Z',
+                'party_intensity': '100',
+            },
+            '2000-01-02T030405Z': {
+                'parcel_id': '2000-01-02T030405Z',
+                'party_intensity': '1',
+            },
+        }
+        dsd.update_metrics(initial)
+        assert dsd.read_metrics() == initial
+
+        updates = {
+            '1999-01-02T030405Z': {
+                'parcel_id': '1999-01-02T030405Z',
+                'party_intensity': '9000',
+                'bugs': '1000000000',
+            },
+            '2020-01-02T030405Z': {
+                'parcel_id': '2020-01-02T030405Z',
+                'bugs': '2000000000',
+            },
+        }
+        dsd.update_metrics(updates)
+        expected = {
+            '1999-01-02T030405Z': {
+                'parcel_id': '1999-01-02T030405Z',
+                'party_intensity': '9000',
+                'bugs': '1000000000',
+            },
+            '2000-01-02T030405Z': {
+                'parcel_id': '2000-01-02T030405Z',
+                'party_intensity': '1',
+                'bugs': '',
+            },
+            '2020-01-02T030405Z': {
+                'parcel_id': '2020-01-02T030405Z',
+                'party_intensity': '',
+                'bugs': '2000000000',
+            },
+        }
+        assert dsd.read_metrics() == expected
