143528fb996fa86b98e17710c05f4100740eee66
Jacob Williams <jacobaw@gmail.com>
2020-04-08 20:38:30 -0700

    finish process command

--- src/export_manager/cli.py
+++ src/export_manager/cli.py
@@ -14,24 +14,23 @@ def process(args):
     status = 0
     for path in args.path:
         ds = DatasetDir(path)
-        if not ds.is_due():
-            continue
 
-        parcel_id = dataset.new_parcel_id()
-        try:
-            ds.run_export(parcel_id)
-        except Exception as e:
-            status = 2
-            print(f'export failed for {path}', file=sys.stderr)
-            print(e, file=sys.stderr)
+        if ds.is_due():
+            parcel_id = dataset.new_parcel_id()
+            try:
+                ds.run_export(parcel_id)
+            except Exception as e:
+                status = 2
+                print(f'export failed for {path}', file=sys.stderr)
+                print(e, file=sys.stderr)
 
-        try:
-            metrics = ds.collect_metrics(parcel_id)
-            ds.update_metrics({parcel_id: metrics})
-        except Exception as e:
-            print(f'metrics failed for {path} parcel_id={parcel_id}',
-                  file=sys.stderr)
-            print(e, file=sys.stderr)
+            try:
+                metrics = ds.collect_metrics(parcel_id)
+                ds.update_metrics({parcel_id: metrics})
+            except Exception as e:
+                print(f'metrics failed for {path} parcel_id={parcel_id}',
+                      file=sys.stderr)
+                print(e, file=sys.stderr)
 
         try:
             ds.clean()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+from freezegun import freeze_time
 from git import Repo
 from pathlib import Path
 from tempfile import TemporaryDirectory
@@ -29,6 +30,30 @@ def test_init_git():
         assert repo.head.commit.message == '[export_manager] initialize'
 
 
+def test_process():
+    with TemporaryDirectory() as rawpath:
+        path = Path(rawpath)
+        config = """
+        interval = "1 day"
+        cmd = "touch $PARCEL_PATH.txt"
+        """
+        path.joinpath('config.toml').write_text(config)
+        times = [f'2000-01-0{d+1}T000000Z' for d in range(9)]
+        for time in times:
+            with freeze_time(time):
+                assert cli.main(['process', rawpath]) == 0
+        assert sorted([f.stem for f in path.glob('data/*.txt')]) == times
+        assert (len(path.joinpath('metrics.csv').read_text().splitlines())
+                == 10)
 
+        # don't produce another export before it's due
+        with freeze_time('2000-01-09T120000Z'):
+            assert cli.main(['process', rawpath]) == 0
+        assert sorted([f.stem for f in path.glob('data/*.txt')]) == times
 
-
+        # do clean old data even when no export is due
+        config += 'keep = 3'
+        path.joinpath('config.toml').write_text(config)
+        with freeze_time('2000-01-09T120000Z'):
+            assert cli.main(['process', rawpath]) == 0
+        assert sorted([f.stem for f in path.glob('data/*.txt')]) == times[6:]
