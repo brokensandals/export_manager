de16fbb4c5f5e1b523384e5593907427bb8d6e90
Jacob Williams <jacobaw@gmail.com>
2020-04-08 18:17:44 -0700

    init cli

--- src/export_manager/cli.py
+++ src/export_manager/cli.py
@@ -1,2 +1,26 @@
-def main():
-    pass
+import argparse
+from export_manager.dataset import DatasetDir
+
+
+def init(args):
+    for path in args.path:
+        DatasetDir(path).initialize(git=args.git)
+
+
+def main(args=None):
+    parser = argparse.ArgumentParser()
+    parser.set_defaults(func=None)
+
+    subs = parser.add_subparsers(title='Commands')
+
+    p_init = subs.add_parser('init', help='initialize new dataset dirs')
+    p_init.add_argument('path', nargs='+', help='dataset dir path')
+    p_init.add_argument('-g', '--git', action='store_true',
+                        help='initialize a git repo')
+    p_init.set_defaults(func=init)
+
+    args = parser.parse_args(args)
+    if not args.func:
+        parser.print_help()
+        return
+    args.func(args)
---
+++ tests/test_cli.py
@@ -0,0 +1,29 @@
+from git import Repo
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from export_manager import cli
+
+
+def test_init():
+    with TemporaryDirectory() as rawpath:
+        path1 = Path(rawpath).joinpath('alpha')
+        path2 = Path(rawpath).joinpath('beta')
+        cli.main(['init', str(path1), str(path2)])
+        assert path1.is_dir()
+        assert path1.joinpath('config.toml').is_file()
+        assert path1.joinpath('metrics.csv').is_file()
+        assert path2.is_dir()
+        assert path2.joinpath('config.toml').is_file()
+        assert path2.joinpath('metrics.csv').is_file()
+
+
+def test_init_git():
+    with TemporaryDirectory() as rawpath:
+        path = Path(rawpath).joinpath('gamma')
+        cli.main(['init', '-g', str(path)])
+        assert path.is_dir()
+        assert path.joinpath('config.toml').is_file()
+        assert path.joinpath('metrics.csv').is_file()
+        assert path.joinpath('.gitignore').is_file()
+        repo = Repo(str(path))
+        assert repo.head.commit.message == '[export_manager] initialize'
