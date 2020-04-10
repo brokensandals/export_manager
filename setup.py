9052fb8f1457f38d300cd046314dd05543d9761d
Jacob Williams <jacobaw@gmail.com>
2020-04-10 15:26:39 -0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="export_manager",
-    version="0.0.1",
+    version="0.0.2",
     author="Jacob Williams",
     author_email="jacobaw@gmail.com",
     description="A tool for managing automated exports of personal data",
